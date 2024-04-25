867b01a7ce3ab2c0997a0fa7fde8c7d8300a99b7
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-25 14:17:01 -0400

    add employees class

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,10 +1,49 @@
 class Employees {
+  _salary;
+  _isHired;
+  static _allEmployees = [];
 
+  constructor(name, position, salary) {
+    this.name = name;
+    this.position = position;
+    this._salary = salary;
+    this._isHired = true;
+    Employees._allEmployees.push(this);
+  }
 
+  getSalary() {
+    return this._salary;
+  }
 
-    
+  setSalary(amount) {
+    if (amount < 0) {
+      throw new Error('Salary cannot be negative');
+    }
+    this._salary = amount;
+  }
+
+  getStatus() {
+    return this._isHired;
+  }
+
+  setStatus(command) {
+    if (command === 'hire') {
+      this._isHired = true;
+    } else if (command === 'fire') {
+      this._isHired = false;
+    }
+  }
+
+  static getEmployees() {
+    return Employees._allEmployees;
+  }
+
+  static getTotalPayroll() {
+    return Employees._allEmployees.reduce(
+      (total, employee) => total + employee._salary,
+      0
+    );
+  }
 }
 
-module.exports = {
-    Employees,
-}
+module.exports = Employees;
