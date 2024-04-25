f406425b7a8d6d47c59e6668a88c2e9af6b3a988
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-25 14:17:09 -0400

    ass manager class

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,10 +1,20 @@
-class Manager {
+const Employees = require('./Employees');
 
+class Manager extends Employees {
+  _employeesManaged;
+  constructor(name, position, salary, department) {
+    super(name, position, salary);
+    this.department = department;
+    this._employeesManaged = [];
+  }
 
+  getEmployeesManaged() {
+    return this._employeesManaged;
+  }
 
-    
+  setEmployeesManaged(employee) {
+    this._employeesManaged.push(employee);
+  }
 }
 
-module.exports = {
-    Manager,
-}
+module.exports = Manager;
