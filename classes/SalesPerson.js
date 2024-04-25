c5a7b7595d7c40ea026b3e6a050606fd62723e68
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-25 14:17:19 -0400

    add salesPerson class

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,26 @@
-class SalesPerson {
+const Employees = require('./Employees');
 
+class SalesPerson extends Employees {
+  _totalSales;
+  constructor(name, position, salary, clients) {
+    super(name, position, salary);
+    this.clients = clients;
+    this._totalSales = 0;
+  }
 
+  getSalesNumber() {
+    return this._totalSales;
+  }
 
-    
+  makeSale(amount) {
+    this._totalSales += amount;
+  }
+
+  findClient(client) {
+    return this.clients.find((c) => c === client);
+  }
 }
 
-module.exports = {
-    SalesPerson,
-}
+module.exports = 
+  SalesPerson
+
