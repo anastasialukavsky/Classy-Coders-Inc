1390d24a143f9241b17aa96dc24106f246e7f131
anastasia lukavsky <stacylukavsky@gmail.com>
2024-04-25 14:17:27 -0400

    add SWE class

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,10 +1,18 @@
-class SoftwareEngineer {
+const Employees = require('./Employees');
 
+class SoftwareEngineer extends Employees {
+  constructor(name, position, salary, _programmingLanguages) {
+    super(name, position, salary);
+    this._programmingLanguages = _programmingLanguages;
+  }
 
+  getProgrammingLanguages() {
+    return this._programmingLanguages;
+  }
 
-    
+  setProgrammingLanguages(language) {
+    this._programmingLanguages.push(language);
+  }
 }
 
-module.exports = {
-    SoftwareEngineer,
-}
+module.exports = SoftwareEngineer;
